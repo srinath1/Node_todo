9fa94a5e58d2ada4a614c090bf5e477e1e080145
sri sri <sri sri>
2017-02-20 19:53:47 +0100

    Updated patch

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+var _=require('lodash');
 var express = require('express');
 var bodyParser = require('body-parser');
 var {ObjectID} = require('mongodb');
@@ -66,6 +67,36 @@ app.delete('/todos/:id', (req, res) => {
     res.status(400).send();
   });
 });
+
+app.patch('/todos/:id',(req,res)=>{
+	
+	var id=req.params.id;
+	var body=_.pick(req.body,['text','completed']);
+	if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  };
+  if(_.isBoolean(body.completed)&& body.completed){
+	  body.completedAt=new Date().getTime();
+	    
+	  
+  }
+	else{
+		body.completed=false;
+		body.completedAt=null;
+	}
+	Todo.findByIdAndUpdate(id,{$set:body},{new:true}).then((todo)=>{
+		if(!todo){
+		 return res.status(400).send();
+		}
+	res.send({todo});
+	}
+	).catch((e)=>{
+		 return res.status(400).send();
+		
+	})
+	
+	
+})
 
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
