cf33ccf333854ef599e794851f2fc79e6aa67c9c
sri sri <sri sri>
2017-02-21 19:04:06 +0100

    Final upadte includes test database

--- server/server.js
+++ server/server.js
@@ -1,3 +1,16 @@
+
+
+var env=process.env.NODE_ENV||'development';
+console.log('**************',env)
+if(env==='development'){
+	process.env.PORT=3000;
+	process.env.MONGODB_URI ='mongodb://localhost:27017/TodoApp';
+}else if(env==='test'){
+	process.env.PORT=3000;
+	process.env.MONGODB_URI ='mongodb://localhost:27017/TodoApptest';
+	
+}
+	
 var _=require('lodash');
 var express = require('express');
 var bodyParser = require('body-parser');
